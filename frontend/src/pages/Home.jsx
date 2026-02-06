668486be87a60a5aa49b80826902726802332345
Muhamed Razik <muhamedrazik061@gmail.com>
2026-02-06 14:20:53 +0000

    fix: update vite config to let "@" to be used instead of "src" in import

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,6 +18,7 @@ import {
   Star,
   ArrowRight
 } from 'lucide-react';
+import testimg from "@/assets/react.svg"
 
 function Home() {
   // Naavika Theme
@@ -274,7 +275,7 @@ function Home() {
               </div>
               <div className="relative mt-12">
                 <motion.img 
-                  src="https://images.unsplash.com/photo-1559839734-2b71ea197ec2?w=600&h=700&fit=crop" 
+                  src={testimg} 
                   alt="Medical Professional" 
                   className="rounded-2xl shadow-2xl w-full max-w-md mx-auto lg:mx-0"
                   whileHover={{ scale: 1.02 }}
