5a488c84f87a9b9244e90b830a77a974b1ccadbf
abhina1289 <abhinarajan865@gmail.com>
2026-02-21 09:30:33 +0530

    "new addition"

--- frontend/src/pages/BookAppointment.jsx
+++ frontend/src/pages/BookAppointment.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { toast } from "sonner";
+import { FaWhatsapp } from "react-icons/fa";
 import {
   Calendar,
   Clock,
